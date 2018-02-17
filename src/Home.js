bfe566d46c569946bbb09bc9f89ee33978700550
Eric Dulcet <etdulcet@gmail.com>
2018-02-17 16:55:42 -0500

    init commit

---
+++ src/Home.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './Home.css';
+
+class Home extends Component {
+  render() {
+    return (
+      <div className="grid-container">
+        <div className="box body">
+          <h1>Eric Dulcet</h1>
+          <h3>Developer & Designer</h3>
+        </div>
+        <div className="box upper-left-corner">About</div>
+        <div className="box upper-right-corner">Contact</div>
+        <div className="box lower-left-corner">Work</div>
+        <div className="box lower-right-corner">Projects</div>
+      </div>
+    );
+  }
+}
+
+export default Home;
