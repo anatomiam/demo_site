1209a794106b9ed2140580cb4810ae192a4b28dd
Eric Dulcet <etdulcet@gmail.com>
2018-02-22 10:56:53 -0500

    added links for projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,6 +3,15 @@ import React from "react";
 const Projects = () => (
   <div className="box body">
     <h1>Projects</h1>
+    <h2>
+      <a href="http://bio.edulcet.local">Bio</a>
+    </h2>
+    <h2>
+      <a href="http://haiku.edulcet.local">Haiku</a>
+    </h2>
+    <h2>
+      <a href="http://blink.edulcet.local">Blink</a>
+    </h2>
   </div>
 );
 
