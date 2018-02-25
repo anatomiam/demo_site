53aa786af53f5119fdc299ae3dd58996e0089e8a
Eric Dulcet <etdulcet@gmail.com>
2018-02-25 17:12:32 -0500

    renamed file path

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -15,7 +15,7 @@ const Navigation = () => (
     <Link to="/projects" className="navigation-items">
       <h3>Projects</h3>
     </Link>
-    <a href="/static/Eric_Dulcet_Resume.pdf" className="navigation-items">
+    <a href="/s/Eric_Dulcet_Resume.pdf" className="navigation-items">
       <h3>Resume</h3>
     </a>
   </div>
