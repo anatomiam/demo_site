9ffc84ee18ece011841ef15f37b1330e3c88a4a5
Eric Dulcet <etdulcet@gmail.com>
2018-03-03 07:24:39 -0500

    switched around links, added favicon

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -9,11 +9,11 @@ const Navigation = () => (
     <Link to="/about" className="navigation-items">
       <h3>About</h3>
     </Link>
-    <Link to="/contact" className="navigation-items">
-      <h3>Contact</h3>
-    </Link>
     <Link to="/projects" className="navigation-items">
       <h3>Projects</h3>
+    </Link>
+    <Link to="/contact" className="navigation-items">
+      <h3>Contact</h3>
     </Link>
     <a href="/s/Eric_Dulcet_Resume.pdf" className="navigation-items">
       <h3>Resume</h3>
